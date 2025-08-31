d370a55692024e4cb199873e078657d516b3ba57
Ler44ikk92 <test@example.com>
2025-08-31 15:52:20 +0400

    files for 5 step

---
+++ app/src/main/java/hexlet/code/Parser.java
@@ -0,0 +1,21 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+
+
+public class Parser {
+
+
+
+    public static Map<String, Object> parser(String content) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map
+                = mapper.readValue(content, new TypeReference<Map<String,Object>>(){});
+        return map;
+    }
+
+}
